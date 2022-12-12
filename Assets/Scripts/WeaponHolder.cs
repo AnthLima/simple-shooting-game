f90236de38dfcc134228951099cf31e6fa76aba7
anthony.lima <anthony.lima@cbyk.com.br>
2022-12-12 00:35:16 -0300

    [feat][main]: Add new movimentation and rotation of gun

--- Assets/Scripts/WeaponHolder.cs
+++ Assets/Scripts/WeaponHolder.cs
@@ -15,5 +15,20 @@ public class WeaponHolder : MonoBehaviour
         var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         var angle =  Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.eulerAngles = new Vector3(0, 0, angle);
+
+        Vector3 playerToMouseDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - player.position;
+        playerToMouseDir.z = 0;
+        transform.position = player.position + (offset * playerToMouseDir.normalized);
+
+        Vector3 localScale = Vector3.one;
+        if(angle > 90 || angle < -90) 
+        {
+            localScale.y = -1f;
+        }else 
+        {
+            localScale.y = 1f;
+        }
+
+        transform.localScale = localScale;
     }
 }
